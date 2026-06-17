104e2c4f92a9c32736cc4d08df75fadfa32705b9
Juanes068 <trianajuan95@gmail.com>
2026-06-16 22:48:11 -0500

    add setting to settings.py

--- havens/settings.py
+++ havens/settings.py
@@ -136,3 +136,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GRAPHENE = {
     'SCHEMA': 'havens.schema.schema'
 }
+
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
