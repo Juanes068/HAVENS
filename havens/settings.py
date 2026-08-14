3560c605436e5c67ddcd1bf30b0fe1fc909f4540
Juanes068 <trianajuan95@gmail.com>
2026-08-14 06:58:26 -0500

    update

--- havens/settings.py
+++ havens/settings.py
@@ -164,5 +164,5 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 EMAIL_TIMEOUT = int(os.getenv('EMAIL_TIMEOUT', '10'))
 
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Havens <joinhavens@gmail.com>') #TODO TODO:change the email adress
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Havens <welcome@havens.app>')
 SERVER_EMAIL = os.getenv('SERVER_EMAIL', DEFAULT_FROM_EMAIL)
